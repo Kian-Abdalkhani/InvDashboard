af0234b002586f907cea2962f2fe7b7814b5b4c3
Kian Abdalkhani <102893487+Kian-Abdalkhani@users.noreply.github.com>
2024-08-05 22:04:33 -0700

    misc removals/restructures

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages  # Assuming setuptools is installed (pip install setuptools)
-
-setup(
-  name='inv_dashboard',  # Replace with your project name
-  version='0.1.0',  # Replace with your version number
-  packages=find_packages(),  # Automatically find packages
-)
